55777d1fc2b3e360e8b96d15abf9189def1900e4
Ashish Verma <7ashishverma7@gmail.com>
2017-12-18 15:05:13 -0500

    update

--- js/javascript.js
+++ js/javascript.js
@@ -16,6 +16,44 @@ const gradeMap = {
 
 function checkGrades() {
   final.textContent = [...inputs].reduce((total, grade) => {
-    return total += gradeMap[grade.value.toUpperCase()]/4;
+    total += gradeMap[grade.value.toUpperCase()]/4;
+    return Math.round(total * 100) / 100;
   }, 0);
 }
+
+
+// THE FOLLOWING CODE INSERTS THE DATA INTO MYSQL
+// WE WILL HAVE TO PROVIDE THE RESPECTIVE username, password etc. TO GIVE SUITABLE DATABASE CONNECTION
+
+
+/*
+var express = require('express');
+var router = express.Router();
+var mysql = require('mysql');
+var con = mysql.createConnection({
+     host: "localhost",
+     user: "root",
+     password: "",
+     database: "test"
+});
+
+router.post('/submitform', function(req, res, next) {
+    console.log(req.body.prof_id);
+    console.log(req.body.stuid_id);
+    console.log(req.body.comments);
+    con.connect(function(err) {
+  if (err) throw  err;
+  console.log("connected");
+  var sql = "INSERT INTO `form`(`prof_id`,`stud_id`, `comments`) VALUES ('"+req.body.prof_id+"','"+req.body.stud_id+"','"+req.body.comments+"')";
+  con.query(sql, function(err, result)  {
+   if(err) throw err;
+   console.log("table created");
+  });
+});
+
+  res.render('index', { title: 'Express' });
+});
+
+
+module.exports = router;
+*/
